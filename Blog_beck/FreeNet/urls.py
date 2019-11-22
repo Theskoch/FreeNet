3134219a44a5105d02e1e4edd9835637f28e2da2
TheSkoCh <geimer.repgas@mail.ru>
2019-11-22 13:14:47 +0300

    говно лайки без защиты очка и с ебучим обновление страницы зато пашет

--- Blog_beck/FreeNet/urls.py
+++ Blog_beck/FreeNet/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    url(r'^post/addlikes/(?P<post_id>[0-9]+)/$', views.addlike, name='post')
 ]
--- Blog_beck/FreeNet/views.py
+++ Blog_beck/FreeNet/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
+from django.http import HttpResponseRedirect
+
+coof_like = 10000 #для тебя степ количество прибавляемых лайков при нажатии если поставиш минус лайки отниматься будут
 
 def main(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'FreeNet/_post.html', {'posts': posts})
+
+
+def addlike(request, post_id):
+    post_like = get_object_or_404(Post, id=post_id)  # возвращает id статьи или 404.
+    post_like.like += coof_like # Прибавляет единицу 
+    post_like.save() # сохраняет
+    return HttpResponseRedirect('/') # делает редирект на ту же страницу
